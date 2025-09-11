515b6e6f19e42fd207aa382b45c71e6d9f009b3f
emanespitia <eespiti@jacksonville.edu>
2025-09-11 15:56:04 -0400

    Initial Commit

---
+++ pract2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  pract2
+//
+//  Created by emanuel espitia on 9/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+  /*    let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+    let firstView = UIView(frame: firstFrame)
+    firstView.backgroundColor = UIColor.blue
+    view.addSubview(firstView)
+        
+    let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+    let secondView = UIView(frame: secondFrame)
+    secondView.backgroundColor = UIColor.green
+    firstView.addSubview(secondView)
+*/
+      
+      
+    
+    }
+
+
+}
+
---
+++ pract2Tests/pract2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  pract2Tests.swift
+//  pract2Tests
+//
+//  Created by emanuel espitia on 9/4/25.
+//
+
+import Testing
+@testable import pract2
+
+struct pract2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
