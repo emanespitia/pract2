1d7efb30656456945c5d0033dae1643ccacf135a
emanespitia <eespiti@jacksonville.edu>
2025-09-27 00:24:45 -0400

    all challanges complete

--- pract2/ConversionViewController.swift
+++ pract2/ConversionViewController.swift
@@ -7,27 +7,87 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
 
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+
+    
   override func viewDidLoad() {
         super.viewDidLoad()
       
         print("ConversionViewController loaded its view.")
-        // Do any additional setup after loading the view.
-   
-  /*    let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-    let firstView = UIView(frame: firstFrame)
-    firstView.backgroundColor = UIColor.blue
-    view.addSubview(firstView)
+      
+        updateCelsiusLabel()
         
-    let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-    let secondView = UIView(frame: secondFrame)
-    secondView.backgroundColor = UIColor.green
-    firstView.addSubview(secondView)
-*/
-      
-      
+    }
     
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let allowedCharacters = CharacterSet(charactersIn: "0123456789.-")
+        let characterSet = CharacterSet(charactersIn: string)
+        if !allowedCharacters.isSuperset(of: characterSet) {
+            return false
+        }
+
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+        
+    }
+    
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+                fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+            } else {
+                fahrenheitValue = nil
+            }
+        
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
 
 
